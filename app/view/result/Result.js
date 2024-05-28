0ffdcf78083b5a76b5cbec75e76b2c93058007c3
Sercan Arğa <mail@sercanarga.com>
2024-05-28 11:39:23 +0300

    added image modal

--- app/view/result/Result.js
+++ app/view/result/Result.js
@@ -3,6 +3,7 @@ Ext.define('ZirvaPortal.view.result.Result', {
     alias: 'widget.result',
 
     requires: [
+        'Ext.window.Window',
         'Ext.layout.VBox',
         'ZirvaPortal.view.result.components.container.Container'
     ],
@@ -15,6 +16,8 @@ Ext.define('ZirvaPortal.view.result.Result', {
         type: 'container',
         flex: 1,
     },
+
+
 
     items: [{
         flex: 1,
@@ -30,9 +33,30 @@ Ext.define('ZirvaPortal.view.result.Result', {
                 width: 177,
                 height: '90%',
                 alt: 'Image',
-                style: 'border:1px solid #4974af;',
+                style: 'border:1px solid #4974af;cursor:pointer;',
                 bind: {
                     src: '{img_src}'
+                },
+                listeners: {
+                    tap: function() {
+                        var resultViewModel = this.up('result').getViewModel();
+                        let win = new Ext.window.Window({
+                            title: 'Snapshot: ' + resultViewModel.get('title'),
+                            width: 800,
+                            height: 568,
+                            closable: true,
+                            maximizable: true,
+                            items: [{
+                                xtype: 'image',
+                                width: '100%',
+                                height: '100%',
+                                style: 'background-size:contain;',
+                                src: resultViewModel.get('img_src'),
+                            }]
+                        });
+
+                        win.show();
+                    }
                 },
             },{
                 xtype: 'component',
